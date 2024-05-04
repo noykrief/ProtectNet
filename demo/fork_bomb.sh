e59a42ea415191a1ff0748104675866fd2cf11f6
idankalman <166123057+idankalman@users.noreply.github.com>
2024-05-04 13:06:08 +0300

    feat: fork bomb script demo for poc (#1)
    * feat: fork bomb script demo for PoC

---
+++ demo/fork_bomb.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+create_child_process() {
+	sleep 9999 &
+	child_pid=$!
+	echo "Created child process with PID: $child_pid"
+}
+
+trap 'kill $(jobs -p); exit' SIGINT SIGTERM
+
+while true
+do
+	create_child_process
+	sleep 1
+done
