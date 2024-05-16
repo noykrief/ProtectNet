5fd4dbc8847a306feca48b119f503719b5b3e4fa
nizan <nizan.shookroun@gmail.com>
2024-05-16 14:44:51 +0300

    API working + color print

--- insightsEngine/API/from_agent.py
+++ insightsEngine/API/from_agent.py
@@ -1,13 +1,13 @@
-from flask import Flask, request
-from ChatGPT.generator import generate_insights
+# from flask import Flask, request
+# from ChatGPT.generator import generate_insights
 
-app = Flask(__name__)
+# app = Flask(__name__)
 
-@app.route('/generate', methods=['POST'])
-def ebpf_to_chat():
-    data = request.json
-    generate_insights(data)
-    return 'Data sent successfully', 200
+# @app.route('/generate', methods=['POST'])
+# def ebpf_to_chat():
+#     data = request.json
+#     generate_insights(data)
+#     return 'Data sent successfully', 200
 
-if __name__ == '__main__':
-    app.run(debug=True)
+# if __name__ == '__main__':
+#     app.run(debug=True)
--- insightsEngine/ChatGPT/generator.py
+++ insightsEngine/ChatGPT/generator.py
@@ -8,9 +8,10 @@ import requests
 # from gptcache.manager import manager_factory
 # from gptcache.cache import get_data_manager
 # from gptcache.core import cache, Cache
-from gptcache.adapter import openai
+# from gptcache.adapter import openai
 from gptcache.session import Session
-# from openai import OpenAI
+from gptcache import cache
+from openai import OpenAI
 from dotenv import load_dotenv
 from colorama import Fore
 
@@ -32,15 +33,17 @@ OPENAI_API_KEY=os.getenv("OEPNAI_API_KEY")
 #   post_process_messages_func=temperature_softmax
 #   )
 
+# cache.init()
 # cache.set_openai_key()
 
 # Create ChatGPT Session
-session = Session(name="insightsGenerator")
+# session = Session(name="insightsGenerator")
 
 # The function defines the bot's purpose and sends the data for analysis
 def generate_insights(ebpf_info):
+  client = OpenAI(api_key=OPENAI_API_KEY)
 
-  completion = openai.chatCompletion.create(
+  completion = client.chat.completions.create(
     model="gpt-3.5-turbo-0125",
     messages=[
       {
@@ -53,14 +56,14 @@ def generate_insights(ebpf_info):
         "role": "system",
         "content": "For each data you get, go through these steps: \n"
         "1. Print every log containing a suspicious method that can be a potential security threat.\n"
-        "2. The header of your response should be:\nTime: 'timestamp',\nType: 'log_type',\nHost: 'hostname',\n"
+        "2. The header of your response should be:\nTime: 'timestamp',\nLog Type: 'log_type',\nHost: 'hostname',\n"
         "Severity: LOW/MEDIUM/HIGH/CRITICAL,\nInfo: 'information',\nAction Items: 'action_items'.\n"
         "Severity should be chosen wisely from the list, according to the potential threat of the log."
         "Info should contain your inferred explanation in simple words of what the log says (what is the problem)."
         "Action Items should contain technical steps needed to perform in order to solve the problem (you will provide them)."
         "3. Gather same logs from different hostnames together if the problem looks the same or you infer a wide"
         "security problem. Gather logs the repeat in logs several times on the same hostname.\n The header of these kind of response should be:\nTime: 'timestamp',\n"
-        "Type: 'log_type',\nHost: 'hostname',\nPotential Severity: LOW/MEDIUM/HIGH/CRITICAL,\nLead: 'lead',\nInfo: 'information'.\n"
+        "Log Type: 'log_type',\nHost: 'hostname',\nPotential Severity: LOW/MEDIUM/HIGH/CRITICAL,\nLead: 'lead',\nInfo: 'information'.\n"
         "Potential Severity should be chosen wisely from the list, according to the potential threat you infer might be."
         "Lead should contain a short explanation of all gathered look-alike logs, inffered by you."
         "Info should contain your inferred more detailed information about the potential threat and a suggestion for an action item."
@@ -69,18 +72,18 @@ def generate_insights(ebpf_info):
       },
       {
         "role": "user",
-        "content": [
+        "content": """[
           {
             "Time": "2024-04-15T12:45:23Z",
-            "Type": "System Call",
+            "Log Type": "System Call",
             "Host": "192.168.1.101",
             "Info": "501, execve, /usr/local/bin/suspicious_script.sh"
           }
-        ]
+        ]"""
       },
       {
         "role": "assistant",
-        "content": "Time: 2024-04-15T12:45:23Z\nType: System Call\nHost: 192.168.1.101\nSeverity: HIGH\n"
+        "content": "Time: 2024-04-15T12:45:23Z\nLog Type: System Call\nHost: 192.168.1.101\nSeverity: HIGH\n"
         "Info: User 501 called 'execve' on '/usr/local/bin/suspicious_script.sh', potentially harmful behavior detected.\n"
         "Action Items: 1. Terminate the process assosiated with the system call by running `kill -9 {pid}`.\n"
         "2. Quarantine the file suspicious_script.sh by running `mv /usr/local/bin/suspicious_script.sh /quarantine/`.\n"
@@ -89,46 +92,46 @@ def generate_insights(ebpf_info):
       },
       {
         "role": "user",
-        "content": [
+        "content": """[
           {
             "Time": "2024-04-15T12:50:00Z",
-            "Type": "System Call",
+            "Log Type": "System Call",
             "Host": "192.168.1.105",
             "Info": "Failed SSH connection from PID 1234"
           },
           {
             "Time": "2024-04-15T12:50:26Z",
-            "Type": "System Call",
+            "Log Type": "System Call",
             "Host": "192.168.1.105",
             "Info": "Failed SSH connection from PID 1234"
           },
           {
             "Time": "2024-04-15T12:51:03Z",
-            "Type": "System Call",
+            "Log Type": "System Call",
             "Host": "192.168.1.105",
             "Info": "Failed SSH connection from PID 1234"
           },
           {
             "Time": "2024-04-15T12:51:43Z",
-            "Type": "System Call",
+            "Log Type": "System Call",
             "Host": "192.168.1.105",
             "Info": "Failed SSH connection from PID 1234"
           }
-        ]
+        ]"""
       },
       {
         "role": "assistant",
-        "content": "Time: 2024-04-15T12:51:00Z\nType: System Call\nPotential Severity: CRITICAL\nLead: Multiple login attempts detected "
+        "content": "Time: 2024-04-15T12:51:00Z\nLog Type: System Call\nPotential Severity: CRITICAL\nLead: Multiple login attempts detected "
         "from IP 192.168.1.105\nInfo: Over 20 failed login attempts in the last 5 minutes. Consider investigation the source IP and "
         "applying IP-based blocking or rate limiting.\n To block this IP using iptables, use the following command: `sudo iptables -A INPUT -s 192.168.1.105 -j DROP`"
       },
       {
         "role": "user",
-        "content": ebpf_info
+        "content": str(ebpf_info)
       },
     ],
     # cache_obj=centralizedCache,
-    session=session
+    # session=session
   )
 
   return (
@@ -153,11 +156,9 @@ def main():
   with open("insightsEngine\ChatGPT\ebpf_info.csv", newline="") as csvfile:
     csv_reader = csv.DictReader(csvfile)
     for row in csv_reader:
-      print(row)
       system_calls.append(row)
 
   if system_calls:
-    print(system_calls)
     print("Starting to analyze your data...")
 
     potential_threats = []
