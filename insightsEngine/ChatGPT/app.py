9ffe46b11633058c5dbde22117915fed8029cd2b
nizan <nizan.shookroun@gmail.com>
2024-08-02 17:30:59 +0300

    revert override of log time

--- insightsEngine/ChatGPT/app.py
+++ insightsEngine/ChatGPT/app.py
@@ -33,12 +33,10 @@ def test_event():
         
         if (test_insight(log_type, target)):
 
-            log_time =  datetime.strptime(log_time, "%Y-%m-%dT%H:%M:%S").astimezone(timezone.utc)
-            print(log_time)
-            start_time = (log_time - timedelta(seconds=30)).isoformat().replace("+00:00", "Z")
-            end_time = log_time.isoformat().replace("+00:00", "Z")
+            utc_log_time =  datetime.strptime(log_time, "%Y-%m-%dT%H:%M:%S").astimezone(timezone.utc)
+            start_time = (utc_log_time - timedelta(seconds=30)).isoformat().replace("+00:00", "Z")
+            end_time = utc_log_time.isoformat().replace("+00:00", "Z")
 
-            print(start_time, end_time)
             params = {
                 'query': '{logger="LokiLogger"}' + f'|= `{target}` | json | Log_Type = `{log_type}` | Time = `{log_time}`',
                 'start': start_time,
