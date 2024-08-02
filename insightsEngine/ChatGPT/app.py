327d81e34d97a1efc3ee5eb135e0b3522ee65e1f
nizan <nizan.shookroun@gmail.com>
2024-08-02 16:01:31 +0300

    time format

--- insightsEngine/ChatGPT/app.py
+++ insightsEngine/ChatGPT/app.py
@@ -33,12 +33,14 @@ def test_event():
         
         if (test_insight(log_type, target)):
 
-            utc_log_time =  datetime.strptime(log_time, "%Y-%m-%dT%H:%M:%SZ").replace(tzinfo=timezone.utc).isoformat()
+            utc_log_time =  datetime.strptime(log_time, "%Y-%m-%dT%H:%M:%SZ").replace(tzinfo=timezone.utc)
+            start_time = (utc_log_time - timedelta(seconds=30)).isoformat()
+            end_time = utc_log_time.isoformat()
 
             params = {
                 'query': '{logger="LokiLogger"}' + f'|= `{target}` | json | Log_Type = `{log_type}` | Time = `{log_time}`',
-                'start': utc_log_time,
-                'end': utc_log_time
+                'start': start_time,
+                'end': end_time
             }
 
             headers = {'Content-Type': 'application/x-www-form-urlencoded'}
