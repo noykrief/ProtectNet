8144ac5599fd5a868a004c1bcb40e0e08244d1d9
nizan <nizan.shookroun@gmail.com>
2024-08-09 16:44:14 +0300

    perform delete

--- insightsEngine/ChatGPT/app.py
+++ insightsEngine/ChatGPT/app.py
@@ -37,30 +37,30 @@ def test_event():
 
             utc_log_time =  datetime.strptime(log_time, "%Y-%m-%dT%H:%M:%S").astimezone(timezone.utc)
             start_time = (utc_log_time - timedelta(seconds=30)).isoformat().replace("+00:00", "Z")
-            # end_time = utc_log_time.isoformat().replace("+00:00", "Z")
+            end_time = utc_log_time.isoformat().replace("+00:00", "Z")
 
-            # params = {
-            #     'query': '{logger="LokiLogger"}' + f'|= `{target}` | json | Log_Type = `{log_type}` | Time = `{log_time}`',
-            #     'start': start_time,
-            #     'end': end_time
-            # }
-
-            query_params = {
+            params = {
                 'query': '{logger="LokiLogger"}' + f'|= `{target}` | json | Log_Type = `{log_type}` | Time = `{log_time}`',
-                'start': start_time
+                'start': start_time,
+                'end': end_time
             }
 
+            # query_params = {
+            #     'query': '{logger="LokiLogger"}' + f'|= `{target}` | json | Log_Type = `{log_type}` | Time = `{log_time}`',
+            #     'start': start_time
+            # }
+
             headers = {'Content-Type': 'application/x-www-form-urlencoded'}
-            result = requests.get("http://10.10.248.155:3100/loki/api/v1/query_range", headers=headers, params=query_params)
+            # result = requests.get("http://10.10.248.155:3100/loki/api/v1/query_range", headers=headers, params=query_params)
 
-            resolved = json.loads(result.json()['data']['result'][0]['values'][0][1])
-            resolved['Severity'] = 'Resolved'
+            # resolved = json.loads(result.json()['data']['result'][0]['values'][0][1])
+            # resolved['Severity'] = 'Resolved'
             
-            print(resolved)
+            # print(resolved)
 
-            logger.info(json.dumps(resolved))            
-            # result = requests.post("http://10.10.248.155:3100/loki/api/v1/delete", headers=headers, params=params)
-            # print(result.content)
+            # logger.info(json.dumps(resolved))            
+            result = requests.post("http://10.10.248.155:3100/loki/api/v1/delete", headers=headers, params=params)
+            print(result.content)
             return jsonify({"message": "Loki data marked as solved successfully"}), 201
 
     except Exception as e:
