3868dd337e07647f614babc9f78c1455922391ea
Nizan Shookroun <60652696+nizansho@users.noreply.github.com>
2024-08-12 17:34:00 +0300

    Feat/user feedback (#30)
    * json result
    * delete print of result
    * print content of request to loki
    * query in json
    * convert inside the function
    * replace ast with json loads + remove irrelevant lines
    * remove debug and change params to loki
    * change time format in agent
    * change time format on generator
    * Add resolved log instead of delete
    * Fix/chat accuracy (#31)
    * chat accuracy and log format to chat
    * print output to screen
    * change chat description
    * rephrase chat description
    * change level log to warn
    
    Co-authored-by: Noy Krief <84447984+noykrief@users.noreply.github.com>

--- insightsEngine/ChatGPT/app.py
+++ insightsEngine/ChatGPT/app.py
@@ -1,5 +1,11 @@
 from flask import Flask, request, jsonify
+from generator import test_insight, configure_logger
 from pymongo import MongoClient
+from datetime import datetime, timedelta, timezone
+
+import requests
+import json
+import time
 
 app = Flask(__name__)
 
@@ -15,6 +21,48 @@ def receive_data():
         result = collection.insert_one(json_data)
 
         return jsonify({"message": "Data inserted successfully", "id": str(result.inserted_id)}), 201
+
+    except Exception as e:
+        return jsonify({"message": "An error occurred", "error": str(e)}), 500
+
+@app.route('/test', methods=['GET'])
+def test_event():
+    try:
+        log_time = request.args.get('time')
+        log_type = request.args.get('log_type')
+        target = request.args.get('target')
+        logger = configure_logger()
+        
+        if (test_insight(log_type, target)):
+
+            utc_log_time =  datetime.strptime(log_time, "%Y-%m-%dT%H:%M:%S").astimezone(timezone.utc)
+            start_time = (utc_log_time - timedelta(seconds=30)).isoformat().replace("+00:00", "Z")
+            # end_time = utc_log_time.isoformat().replace("+00:00", "Z")
+
+            # params = {
+            #     'query': '{logger="LokiLogger"}' + f'|= `{target}` | json | Log_Type = `{log_type}` | Time = `{log_time}`',
+            #     'start': start_time,
+            #     'end': end_time
+            # }
+
+            query_params = {
+                'query': '{logger="LokiLogger"}' + f'|= `{target}` | json | Log_Type = `{log_type}` | Time = `{log_time}`',
+                'start': start_time
+            }
+
+            headers = {'Content-Type': 'application/x-www-form-urlencoded'}
+            result = requests.get("http://10.10.248.155:3100/loki/api/v1/query_range", headers=headers, params=query_params)
+
+            resolved = json.loads(result.json()['data']['result'][0]['values'][0][1])
+            resolved['Severity'] = 'Resolved'
+            
+            print(resolved)
+
+            logger.warning(json.dumps(resolved))            
+            # result = requests.post("http://10.10.248.155:3100/loki/api/v1/delete", headers=headers, params=params)
+            # print(result.content)
+            return jsonify({"message": "Log has been resolved successfully"}), 201
+        return jsonify({"message": "Event is still relevant"}), 201
 
     except Exception as e:
         return jsonify({"message": "An error occurred", "error": str(e)}), 500
