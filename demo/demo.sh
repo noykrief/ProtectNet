850d80400577daf2259b52686186ff30832133d3
idankalman <166123057+idankalman@users.noreply.github.com>
2024-08-24 14:20:02 +0300

    Update demo.sh

--- demo/demo.sh
+++ demo/demo.sh
@@ -14,7 +14,7 @@ echo "###########################################################"
 echo "#####      Coppying attack script to remote host      #####"
 echo "###########################################################"
 echo ""
-echo "   $ sudo scp attack.sh demo@10.10.248.158/home/demo/"
+echo "   $ sudo scp attack.sh demo@10.10.248.158:///home/demo/"
 sleep 5
 echo ""
 sshpass -p $PASSWORD sudo scp attack.sh demo@10.10.248.158:///home/demo/
