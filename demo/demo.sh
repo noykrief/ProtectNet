d1260dcbf31c0e1a8d685f0aa6686271bb6104a3
idankalman <166123057+idankalman@users.noreply.github.com>
2024-08-24 17:47:14 +0300

    changing sleep 5 to sleep 3 in attack script

--- demo/demo.sh
+++ demo/demo.sh
@@ -10,23 +10,23 @@ echo "#####          Scanning ports on remote host          #####"
 echo "###########################################################"
 echo ""
 echo "   $ sudo nmap -p 1-30 10.10.248.158"
-sleep 5
+sleep 3
 echo ""
 sudo nmap -p 1-30 10.10.248.158
 echo ""
 echo ""
-sleep 5
+sleep 3
 
 echo "###########################################################"
 echo "#####      Coppying attack script to remote host      #####"
 echo "###########################################################"
 echo ""
 echo "   $ scp attack.sh demo@10.10.248.158:///home/demo/"
-sleep 5
+sleep 3
 echo ""
 sshpass -p $PASSWORD scp attack.sh demo@10.10.248.158:///home/demo/
 echo ""
 echo ""
-sleep 5
+sleep 3
 
 echo -e "$Color_Off"
