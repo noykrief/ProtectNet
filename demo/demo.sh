026c910eca74b717e667aef8ab28dd027e793c5b
idankalman <166123057+idankalman@users.noreply.github.com>
2024-08-24 14:10:48 +0300

    Update demo.sh

--- demo/demo.sh
+++ demo/demo.sh
@@ -10,15 +10,14 @@ echo ""
 echo ""
 sleep 5
 
-
 echo "###########################################################"
-echo "#####        SSH into remote host as demo user        #####"
+echo "#####      Coppying attack script to remote host      #####"
 echo "###########################################################"
 echo ""
-echo "   $ ssh demo@10.10.248.158"
+echo "   $ sudo scp attack.sh demo@10.10.248.158/home/demo/"
 sleep 5
 echo ""
-sshpass -p 1 ssh -t demo@10.10.248.158 'sshpass -p 1 sudo /home/demo/attack.sh'
+sshpass -p $PASSWORD sudo scp attack.sh demo@10.10.248.158/home/demo/
 echo ""
 echo ""
 sleep 5
